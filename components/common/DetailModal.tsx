375bb202893dbe218accd6f62e03bdb929856804
phvdat <phamvandat.hcmut@gmail.com>
2026-02-14 08:51:11 +0700

    update: virtual scroll

--- components/common/DetailModal.tsx
+++ components/common/DetailModal.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import {
@@ -7,15 +6,32 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "../ui/table";
-import { ArbitrageResult } from "@/lib/store/type";
+
+import { ArbitrageResult, ArbitrageTick } from "@/lib/store/type";
+import { List, RowComponentProps } from "react-window";
+
+function Row({
+  index,
+  style,
+  result,
+  history,
+}: RowComponentProps & { result: ArbitrageResult; history: ArbitrageTick[] }) {
+  const h = history[index];
+
+  return (
+    <div style={style} className="grid grid-cols-5 border-b px-4 py-2 text-sm">
+      <div>{new Date(h.ts).toLocaleString("vi-VN")}</div>
+      <div>{h.ratio.toFixed(4)}</div>
+      <div>{h.profit.toFixed(2)}</div>
+      <div>{h?.quantity?.toFixed(2)}</div>
+      <div>
+        {h?.direction === "A_TO_B"
+          ? `${result.exchange1} → ${result.exchange2}`
+          : `${result.exchange2} → ${result.exchange1}`}
+      </div>
+    </div>
+  );
+}
 
 export function DetailModal({
   result,
@@ -24,9 +40,11 @@ export function DetailModal({
   result: ArbitrageResult;
   onClose: () => void;
 }) {
+  const history = result.history?.slice().reverse() ?? [];
+
   return (
     <Dialog open onOpenChange={onClose}>
-      <DialogContent>
+      <DialogContent className="min-w-fit w-3xl">
         <DialogHeader>
           <DialogTitle>
             {result.pair} — {result.exchange1.toUpperCase()} -{" "}
@@ -34,42 +52,37 @@ export function DetailModal({
           </DialogTitle>
         </DialogHeader>
 
-        <div className="max-h-[400px] overflow-y-auto border rounded">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Time</TableHead>
-                <TableHead>Ratio %</TableHead>
-                <TableHead>Profit</TableHead>
-                <TableHead>Quantity</TableHead>
-                <TableHead>Direction</TableHead>˝
-              </TableRow>
-            </TableHeader>
+        <div className="overflow-x-auto">
+          <div className="min-w-[600px] max-h-[600px]">
+            <div className="grid grid-cols-5 bg-muted border-b px-4 py-2 text-sm font-semibold">
+              <div>Time</div>
+              <div>Ratio %</div>
+              <div>Profit</div>
+              <div>Quantity</div>
+              <div>Direction</div>
+            </div>
 
-            <TableBody>
-              {result.history
-                ?.slice()
-                .reverse()
-                .map((h: any, i: number) => (
-                  <TableRow key={i}>
-                    <TableCell>
-                      {new Date(h.ts).toLocaleString("vi-VN", {
-                        day: "2-digit",
-                        month: "2-digit",
-                        year: "numeric",
-                        hour: "2-digit",
-                        minute: "2-digit",
-                        second: "2-digit",
-                      })}
-                    </TableCell>
-                    <TableCell>{h.ratio.toFixed(4)}</TableCell>
-                    <TableCell>{h.profit.toFixed(2)}</TableCell>
-                    <TableCell>{h.last?.quantity?.toFixed(2)}</TableCell>
-                    <TableCell>{h.last?.direction === "A_TO_B" ? `${result.exchange1} -> ${result.exchange2}` : `${result.exchange2} -> ${result.exchange1}`}</TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
+            <List
+              className="md:hidden"
+              rowCount={history.length}
+              rowHeight={48}
+              rowComponent={Row}
+              rowProps={{
+                result,
+                history,
+              }}
+            />
+            <List
+              className="hidden md:block"
+              rowCount={history.length}
+              rowHeight={36}
+              rowComponent={Row}
+              rowProps={{
+                result,
+                history,
+              }}
+            />
+          </div>
         </div>
       </DialogContent>
     </Dialog>
